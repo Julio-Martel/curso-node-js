7ca1036cd6de1d8766f2cef982ac506db959e7cc
Diocleniax789 <julioramdom123@gmail.com>
2025-04-16 19:49:28 -0300

    se han agregado comentarios para el uso del modulo process

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-const {suma,resta} = require('./saludo.js');
+/*const {suma} = require('./saludo.js');
 
 console.log(suma(4,6));
-console.log(resta(5,9));
+console.log(resta(5,9));*
+
+console.log(process.memoryUsage());
+console.log(process)*/
+
+// sirve para obtener argumentos desde la terminal
+console.log(process.argv);
+
+// sirve para saber en que s.o estoy
+console.log(process.platform);
